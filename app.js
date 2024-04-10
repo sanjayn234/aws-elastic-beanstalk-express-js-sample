307fd706d1d54e41a68cdca191da724d4ddcef53
Sanjay Nimmala <nimmala.7@TeleperformanceDIBS.com>
2024-04-10 14:18:14 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const app = express();
 const port = 8080;
 
 <<<<<<< HEAD
-=======
-app.get('/', (req, res) => res.send('Hello Satish!'));
+app.get('/', (req, res) => res.send('Hello AWS!'));
 >>>>>>> 65fee1d346afa2794a2ea3de1365270fbc8f39d6
 
 app.listen(port);
